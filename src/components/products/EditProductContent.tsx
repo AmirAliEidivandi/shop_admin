c58f718eb8fc58c4432401cec881e9d9df427d92
AmirAliEidivandi <amirah648887@gmail.com>
2022-12-04 23:26:11 +0330

    create functions for load file

--- src/components/products/EditProductContent.tsx
+++ src/components/products/EditProductContent.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Content from "../partials/Content";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
-import { FormControl, Grid, InputLabel, MenuItem, Select, TextField } from "@material-ui/core";
+import { FormControl, Grid, InputLabel, MenuItem, Select, TextField, Typography, Divider, Button } from "@material-ui/core";
 import HttpClient from "../../services/Http";
 import { v4 as uuid } from "uuid";
 import Section from "../partials/Section";
@@ -23,10 +23,25 @@ const useStyles = makeStyles((theme: Theme) =>
     })
 );
 
+interface AttributeItem {
+    title: string;
+    slug: string;
+    filterable: boolean;
+    hasPrice: boolean;
+}
+
+interface ProductAttributes {
+    title: string;
+    attributes: AttributeItem[];
+}
+
 const EditProductContent = () => {
     const styles = useStyles();
-    const [categories, setCategories] = useState<CategoryItem[]>([]);
     const api = new HttpClient();
+    const [categories, setCategories] = useState<CategoryItem[]>([]);
+    const [productAttributes, setProductAttributes] = useState<ProductAttributes[]>([]);
+    const [thumbnail, setThumbnail] = useState<File | null>(null);
+    const [gallery, setGallery] = useState<File[]>([]);
 
     useEffect(() => {
         api.get<CategoryItem[]>("api/v1/categories")
@@ -39,7 +54,36 @@ const EditProductContent = () => {
             });
     }, []);
 
-    const updateCategory = (e: React.ChangeEvent) => {};
+    const updateCategory = (e: React.ChangeEvent<{ value: unknown }>) => {
+        api.get<ProductAttributes[]>(`api/v1/categories/${e.target.value}/attributes`)
+            .then((response) => {
+                setProductAttributes(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
+    const updateThumbnail = (file: File) => {
+        setThumbnail(file);
+    };
+
+    const updateGallery = (file: File) => {
+        setGallery((prev: File[]) => {
+            return [...prev, file];
+        });
+    };
+
+    const saveProduct = (e: React.MouseEvent) => {
+        e.preventDefault();
+
+        const form = new FormData();
+        form.append("thumbnail", thumbnail as Blob);
+
+        gallery.forEach((file: File) => {
+            form.append("gallery[]", file as Blob);
+        });
+    };
 
     return (
         <Content title="ویرایش / اضافه کردن محصول">
@@ -58,7 +102,7 @@ const EditProductContent = () => {
             <Grid xs={6}>
                 <FormControl fullWidth className={styles.formRow}>
                     <InputLabel id="category_label">دسته بندی</InputLabel>
-                    <Select labelId="category_label" id="category_label">
+                    <Select labelId="category_label" id="category_label" onChange={updateCategory}>
                         <MenuItem value={0}>دسته بندی را انتخاب کنید</MenuItem>
                         {categories?.map((category: CategoryItem) => (
                             <MenuItem key={category.id} value={category.id}>
@@ -69,38 +113,45 @@ const EditProductContent = () => {
                 </FormControl>
             </Grid>
             <Section title="تصویر شاخص">
-                <ImageInput
-                    onChange={(file: File) => {
-                        console.log(file);
-                    }}
-                />
+                <ImageInput onChange={updateThumbnail} />
             </Section>
             <Section title="گالری تصاویر">
                 <Grid container>
                     <Grid item xs={12} md={4}>
-                        <ImageInput
-                            onChange={(file: File) => {
-                                console.log(file);
-                            }}
-                        />
+                        <ImageInput onChange={updateGallery} />
                     </Grid>
                     <Grid item xs={12} md={4}>
-                        <ImageInput
-                            onChange={(file: File) => {
-                                console.log(file);
-                            }}
-                        />
+                        <ImageInput onChange={updateGallery} />
                     </Grid>
                     <Grid item xs={12} md={4}>
-                        <ImageInput
-                            onChange={(file: File) => {
-                                console.log(file);
-                            }}
-                        />
+                        <ImageInput onChange={updateGallery} />
                     </Grid>
                 </Grid>
             </Section>
-            <Section title="مشخصات محصول"></Section>
+            <Section title="مشخصات محصول">
+                {productAttributes?.map((group: ProductAttributes) => {
+                    return (
+                        <>
+                            <Typography variant="h6">{group.title}</Typography>
+                            <Divider />
+                            {group?.attributes?.map((attribute: AttributeItem) => {
+                                return (
+                                    <>
+                                        <FormControl fullWidth className={styles.formRow}>
+                                            <TextField variant="outlined" label={attribute.title} />
+                                        </FormControl>
+                                    </>
+                                );
+                            })}
+                        </>
+                    );
+                })}
+            </Section>
+            <FormControl fullWidth className={styles.formRow}>
+                <Button onClick={saveProduct} variant="contained" color="primary">
+                    ذخیره محصول
+                </Button>
+            </FormControl>
         </Content>
     );
 };
